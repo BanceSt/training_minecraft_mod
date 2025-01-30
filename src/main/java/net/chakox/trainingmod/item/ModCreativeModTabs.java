4231ba171e1e7bf94be35771eae906f15df178dc
BanceSt <59577061+BanceSt@users.noreply.github.com>
2025-01-30 00:35:29 -0400

    add item and creative tab

---
+++ src/main/java/net/chakox/trainingmod/item/ModCreativeModTabs.java
@@ -0,0 +1,30 @@
+package net.chakox.trainingmod.item;
+
+import net.chakox.trainingmod.TrainingMod;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeModTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TrainingMod.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> TRAINING_TAB = CREATIVE_MODE_TABS.register("training_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SAPPHIRE.get()))
+                    .title(Component.translatable("creativetab.training_tab"))
+                    .displayItems((pParameters, pOutput) -> {
+                        pOutput.accept(ModItems.SAPPHIRE.get());
+                        pOutput.accept(ModItems.RAW_SAPPHIRE.get());
+                        pOutput.accept(Items.ENDER_PEARL);
+                    })
+                    .build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
