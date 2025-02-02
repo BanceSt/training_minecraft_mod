00221af9a06d4da82c3beb77df2932083eba3893
BanceSt <59577061+BanceSt@users.noreply.github.com>
2025-02-01 23:12:25 -0400

    add custom block metal detector

--- src/main/java/net/chakox/trainingmod/item/ModCreativeModTabs.java
+++ src/main/java/net/chakox/trainingmod/item/ModCreativeModTabs.java
@@ -22,6 +22,7 @@ public class ModCreativeModTabs {
                         pOutput.accept(ModItems.SAPPHIRE.get());
                         pOutput.accept(ModItems.RAW_SAPPHIRE.get());
                         pOutput.accept(Items.ENDER_PEARL);
+                        pOutput.accept(ModItems.METAL_DETECTOR.get());
 
                         pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
                         pOutput.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
